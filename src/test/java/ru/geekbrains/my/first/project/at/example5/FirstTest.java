06081a6384b1a49c4b286f499b894dc4e184825d
Antonpavlov <anton.it.pavlov@gmail.com>
2020-06-17 02:28:23 +0300

    отладка прогона

--- src/test/java/ru/geekbrains/my/first/project/at/example5/FirstTest.java
+++ src/test/java/ru/geekbrains/my/first/project/at/example5/FirstTest.java
@@ -1,8 +1,6 @@
 package ru.geekbrains.my.first.project.at.example5;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
@@ -10,7 +8,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import ru.geekbrains.my.first.project.at.example2.Page;
 
 @Story("Навигация")
 public class FirstTest {
@@ -34,9 +31,10 @@ public class FirstTest {
     })
     @Feature("Проверка перехода")
     public void button(String name) {
-        Selenide.open("https://geekbrains.ru/courses");
-        Page page = new Page();
-        page.getNavigation().getButton(name).click();
-        page.getHeader().getTitle().shouldHave(Condition.exactText(name));
+        throw new NullPointerException();
+//        Selenide.open("https://geekbrains.ru/courses");
+//        Page page = new Page();
+//        page.getNavigation().getButton(name).click();
+//        page.getHeader().getTitle().shouldHave(Condition.exactText(name));
     }
 }
